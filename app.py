52698b3eafc00cddc471116e9e7011a585bdf032
Ask1012 <ak1074834@gmail.com>
2025-01-12 02:47:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1038,5 +1038,7 @@ def payment_page():
    return render_template('payment_page.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get the port from the environment variable; default to 5000 if not set
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
     
